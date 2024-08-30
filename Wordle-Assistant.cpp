9b724fa9300233559808912a56dade80b52a71b2
Ean Dodge <145071390+EanDodge@users.noreply.github.com>
2024-08-30 12:06:28 -0400

    added cpp

---
+++ Wordle-Assistant.cpp
@@ -0,0 +1,28 @@
+//Delta Group
+//8/29/2024
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+
+using std::cout; using std::endl; using std::cin;
+using std::vector; using std::string; 
+using std::ifstream; using std::ofstream;
+
+
+int main(){
+    ifstream fin("five_words.txt");
+    
+    vector<string> strs;
+     string myString = getline(fin, strs);
+    int i= 0;
+    while(strs != fin.eof()){
+        strs[i] = myString;
+        myString = getline(fin, strs);
+        ++i;
+    }
+    for(auto i : strs){
+        cout << i<< endl;
+    }
+}
