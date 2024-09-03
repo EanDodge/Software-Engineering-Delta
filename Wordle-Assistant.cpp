f2cbd943f9f4acf6f77a6d4a1aec6439718e5d05
Ean Dodge <145071390+EanDodge@users.noreply.github.com>
2024-09-02 21:31:53 -0400

    updated with comments

--- Wordle-Assistant.cpp
+++ Wordle-Assistant.cpp
@@ -29,11 +29,11 @@ int main(){
     getline(fin, myString);
     while(!fin.eof()){
         //cout << "got here";
-        strs.push_back(myString);
+        strs.push_back(myString); //put all words from words.txt into strs.
         getline(fin, myString);
     }
     //cout << strs.size();
-    string guess = "notes";
+    string guess = "notes"; //always start with notes
     //myErase(strs, 'a');
     //cout << strs.size();
 
@@ -42,14 +42,14 @@ int main(){
     // }
     //.(not found), #(found, not in right place), !(in right place)
     string result = "";
-    vector<char> bank;
+    vector<char> bank; //for letters that wont be deleted
     //cin >> result;
     int i = 0, j = 0;
     while(j != 5){
         cout << "this is your guess '" << guess << "'"<<endl;
         cin >> result;
         if(result[0] == '!' && result[2] == '!' && result[4] == '!' 
-        && result[6] == '!' && result[8] == '!'){
+        && result[6] == '!' && result[8] == '!'){ //if all is correct
             cout << "Good job! See you tomorrow";
             exit(1);
         }
@@ -78,11 +78,11 @@ int main(){
                 --j;
                 flag = true;
             }
-            i+=2;
-            if (flag == true)
+            i+=2; // increase by two for the word, because of a symbol, then letter
+            if (flag == true) // for debug purpose, if want to see bank, then leave
                 break;
         }
-        ++j;
+        ++j; //increase row of wordle
         if(guess != strs[0])
             guess = strs[0];
         else
@@ -95,13 +95,16 @@ int main(){
 
 
 
-void myErasenot(vector<string>& strs, vector<char>& bank, const char& a,const int& place){
+void myErasenot(vector<string>& strs, vector<char>& bank, const char& a,const int& place){ //grey letter case
+                                        //checks a word bank so it doesnt purge words that have double letter (shoot)
+                                        //will purge every word with letter in that position of double letter(#o.o)
+                                        //If false, will purge every word with that letter
     bool flag = false;
     for(auto i: bank){
         if (a == i) //if a is in the word bank, do not delete it from word, just delete words that have it in that spot
             flag = true;
     }
-    if(flag){
+    if(flag){  
         strs.erase(remove_if(strs.begin(), strs.end(),[&a, place](string str)
    {return str[place] == a;}),strs.end());
     }
@@ -110,21 +113,24 @@ void myErasenot(vector<string>& strs, vector<char>& bank, const char& a,const in
    {return str.find(a) != string::npos;}),strs.end());
     }
 }
-void myErasehave(vector<string>& strs, const char& a, const int& place){
+void myErasehave(vector<string>& strs, const char& a, const int& place){ // yellow letter case
+                                                            // deletes all words that have that letter in that spot
+                                                            // deletes all words that dont have that letter at all
     strs.erase(remove_if(strs.begin(), strs.end(),[&a, place](string str)
    {return str[place] == a;}),strs.end());
     strs.erase(remove_if(strs.begin(), strs.end(),[&a](string str)
    {return str.find(a) == string::npos;}),strs.end());
    
 }
-void myEraseplace(vector<string>& strs, const char& a, const int& place){
+void myEraseplace(vector<string>& strs, const char& a, const int& place){// this is the green letter case
+                                                         // Will purge words that dont have letter in that position
     //myErasehave(strs,a);
     strs.erase(remove_if(strs.begin(), strs.end(),[&a, place](string str)
    {return str[place] != a;}),strs.end());
 }
 
-void print(vector<string> strs){
-    for (auto i: strs){
+void print(vector<string> strs){ //Will print all words left in strs
+    for (auto i: strs){ 
         cout << i << endl;
     }
 }
