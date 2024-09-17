9adf3859a9b8d28d21af20d2309815ad7a01eb0b
Brent Kent <bwarrin3@kent.edu>
2024-09-17 18:28:54 -0400

    Add Camera that follows player

--- Main Project/sketch.js
+++ Main Project/sketch.js
@@ -1,11 +1,22 @@
 let player = new Player(250, 250);
 
 function setup() {
-  createCanvas(500, 500);
+  createCanvas(500, 500, WEBGL);
+
+  //camera to follow player
+  cam = createCamera();
 }
 
 function draw() {
   background("lightblue");
+
   player.drawPlayer();
   player.movePlayer();
+
+  //moves cam to centered on player, z=800 default
+  cam.setPosition(player.x, player.y, 800);
+
+  //random object to show screen move
+  fill(255, 0, 0);
+  ellipse(100, 100, 50, 50);
 }
