2b1c7c2f89a1b10facb8d9d952c7a87081b762bc
Matt Crosby <mattcrosby01@gmail.com>
2024-11-04 19:26:40 -0500

    uploading fixes to upgrade island

--- Main Project/sketchPirate.js
+++ Main Project/sketchPirate.js
@@ -1,89 +1,81 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const canvas = document.getElementById('gameCanvas');
-    const ctx = canvas.getContext('2d');
+// document.addEventListener('DOMContentLoaded', () => {
+//     const canvas = document.getElementById('gameCanvas');
+//     const ctx = canvas.getContext('2d');
 
-    const pirateImgUrl = './assets/pirate.png';
-    const seaImgUrl = './assets/sea.png';
-    const backgroundImgUrl = './assets/upgradeislandAlt.png';
+//     const pirateImgUrl = './assets/pirate.gif';
+//     const seaImgUrl = './assets/sea.png';
+//     const backgroundImgUrl = './assets/upgradeislandAlt.png';
 
-    const pirate = new Pirate(750, 630, pirateImgUrl);
+//     const pirate = new Pirate(750, 630, pirateImgUrl);
 
-    const seaImage = new Image();
-    const background = new Image();
+//     const seaImage = new Image();
+//     const background = new Image();
     
-    background.src = backgroundImgUrl;
-    seaImage.src = seaImgUrl;
+//     background.src = backgroundImgUrl;
+//     seaImage.src = seaImgUrl;
 
-    const puzzle = new islandObject(0,0);
+//     function resizeCanvas(){
+//         canvas.width = window.innerWidth;
+//         canvas.height = window.innerHeight;
+//         drawScene();
+//     }
 
-    function resizeCanvas(){
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
-        drawScene();
-    }
-
-    function drawScene(){
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.drawImage(seaImage, 0, 0, canvas.width , canvas.height);
+//     function drawScene(){
+//         ctx.clearRect(0, 0, canvas.width, canvas.height);
+//         ctx.drawImage(seaImage, 0, 0, canvas.width , canvas.height);
         
-        //need to scale the island.
-        //default size is not big enough for entire browser scree
-        let scaleFactor = 1.5;
-        const newWidth = background.width * scaleFactor;
-        const newHeight = background.height * scaleFactor;
+//         //need to scale the island.
+//         //default size is not big enough for entire browser scree
+//         let scaleFactor = 1.5;
+//         const newWidth = background.width * scaleFactor;
+//         const newHeight = background.height * scaleFactor;
 
     
-        const centerX = (canvas.width - newWidth) / 2;
-        const centerY = (canvas.height - newHeight) / 2;
-        ctx.drawImage(background, centerX, centerY, newWidth, newHeight);
-        pirate.draw(ctx);
-    }
+//         const centerX = (canvas.width - newWidth) / 2;
+//         const centerY = (canvas.height - newHeight) / 2;
+//         ctx.drawImage(background, centerX, centerY, newWidth, newHeight);
+//         pirate.draw(ctx);
+//     }
 
-    //runs once
-    seaImage.onload = () => {
-        background.onload = () => {
-        resizeCanvas()
-        window.addEventListener('resize', resizeCanvas);
-        gameLoop();
-        circle(0,0, 50);
-    };
-};
+//     seaImage.onload = () => {
+//         background.onload = () => {
+//         resizeCanvas()
+//         window.addEventListener('resize', resizeCanvas);
+//         gameLoop();
+//         circle(0,0, 50);
+//     };
+// };
 
-    //runs constantly
-    function gameLoop() {
-        drawScene();
-        requestAnimationFrame(gameLoop);
+//     function gameLoop() {
+//         drawScene();
+//         requestAnimationFrame(gameLoop);
+//     }
 
-        // if(pirate.isColliding(puzzle)) {
-        //     window.location.href = 'nurikabe.html'
-        // }
-    }
+//     //gameLoop();
 
+// function onKeyPress(e){
+//     switch(e.key){
+//         case 'ArrowUp':
+//         case 'w':
+//             pirate.move(0, -10);
+//             break;
+//         case 'ArrowDown':
+//         case 's':
+//             pirate.move(0, 10);
+//             break;
+//         case 'ArrowLeft':
+//         case 'a':
+//             pirate.setDirection('left');
+//             pirate.move(-10, 0);                       
+//             break;
+//         case 'ArrowRight':
+//         case 'd':
+//             pirate.setDirection('right');
+//             pirate.move(10, 0);
+//             break;
 
-function onKeyPress(e){
-    switch(e.key){
-        case 'ArrowUp':
-        case 'w':
-            pirate.move(0, -10);
-            break;
-        case 'ArrowDown':
-        case 's':
-            pirate.move(0, 10);
-            break;
-        case 'ArrowLeft':
-        case 'a':
-            pirate.setDirection('left');
-            pirate.move(-10, 0);                       
-            break;
-        case 'ArrowRight':
-        case 'd':
-            pirate.setDirection('right');
-            pirate.move(10, 0);
-            break;
+// //     }
+// // }
 
-    }
-}
-
-window.addEventListener('keydown', onKeyPress)
-});
-
+// // window.addEventListener('keydown', onKeyPress)
+// // });
